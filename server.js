a374f188744278152f239e901ef4307b0ba202f0
koppojuTrinaini <koppojutrinaini@gmail.com>
2026-05-22 11:08:52 +0530

    final smtp fix

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/send-email", async (req, res) => {
 
         const transporter = nodemailer.createTransport({
 
-            host: "smtp.gmail.com",
+            host: "smtp-relay.brevo.com",
 
             port: 587,
 
@@ -42,10 +42,6 @@ app.post("/send-email", async (req, res) => {
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
-            },
-
-            tls: {
-                rejectUnauthorized: false
             }
 
         });
